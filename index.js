aee23b5592155b80ca0603e45fabfd50f154d455
Denys Panov <denys.panov@senacor.com>
2021-04-18 19:45:01 +0200

    v0.1.1

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-exports.sayHello = function() {
-  return 'Hello from Kurisu Makise!';
+let Exchange = {
+  EUR: {
+    USD: 1.20
+  },
+  USD: {
+    EUR: 0.83
+  }
+};
+
+const convert = async (from, to, amount) => {
+  return Exchange[from] && Exchange[from][to] ? amount * Exchange[from][to] : amount
 }
+
+export {convert}
